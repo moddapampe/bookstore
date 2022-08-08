f4cce0d164fabfc8920fce1fcb663332762818ef
modda <support@moddapampe.de>
2022-08-08 11:04:34 +0200

    add bookmark-btn

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <button :class="styling" @click="$emit('btn-clicked')">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  emits: ["btn-clicked"],
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      default: "primary",
+      validator(propValue) {
+        return ["primary", "secondary"].includes(propValue);
+      },
+    },
+  },
+  computed: {
+    styling() {
+      return ["btn", `btn--${this.variant}`];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  border: 3px solid transparent;
+  border-radius: 3px;
+  padding: 5px;
+  cursor: pointer;
+  width: 100%;
+  display: block;
+}
+
+.btn--primary {
+  border-color: var(--primary-dark);
+  background-color: var(--primary);
+  color: #fff;
+}
+
+.btn--primary:hover {
+  border-color: var(--primary);
+  background-color: var(--primary-dark);
+}
+
+.btn--secondary {
+  border-color: var(--primary);
+  background-color: transparent;
+  color: var(--primary);
+}
+
+.btn--secondary:hover {
+  background-color: var(--primary);
+  color: #fff;
+}
+</style>
---
+++ src/components/BookListRow.vue
@@ -0,0 +1,43 @@
+<template>
+  <tr class="table-item__table-row">
+    <td>{{ title }}</td>
+    <td>{{ isbn }}</td>
+    <td>
+      <BaseButton
+        :text="bookMarkedBtnText"
+        variant="secondary"
+        @btn-clicked="handleBookMarkButtonClick"
+      />
+    </td>
+  </tr>
+</template>
+
+<script>
+import BaseButton from "@/components/BaseButton.vue";
+export default {
+  components: {
+    BaseButton,
+  },
+  props: {
+    title: String,
+    isbn: String,
+    isBookmarked: Boolean,
+  },
+  computed: {
+    bookMarkedBtnText() {
+      return this.isBookmarked ? "Remove Bookmark" : "Add Bookmark";
+    },
+  },
+  methods: {
+    handleBookMarkButtonClick() {
+      this.$emit("bookmark-clicked", this.isbn);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.table-item__table td {
+  padding: 12px 15px;
+}
+</style>
